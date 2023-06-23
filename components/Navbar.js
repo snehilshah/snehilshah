71fc3f730ed885f19299fd4eb125fdd0d5795a83
srshah27 <snehilshah27@gmail.com>
2023-06-23 05:32:50 +0530

    Style :art: :ResponsiveNavbar Added Animation to Scroll Top & Bottom :fire: Needed to change Vertical Navbar: absolute -> fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,21 @@ export default function Navbar() {
 	const links = ['Home', 'About', 'Experience', 'Contact']
 	const [Nav, setNav] = useState(false)
 
+	useEffect(() => {
+		let lastScrollTop = 0
+		let navbar = document.getElementById('nav')
+		window.addEventListener('scroll', () => {
+			let scrollTop = window.scrollY || document.documentElement.scrollTop
+			if(scrollTop > lastScrollTop){
+				navbar.style.top = '-100px'
+			}
+			else{
+				navbar.style.top = '0'
+			}
+			lastScrollTop = scrollTop
+		})
+	})
+
 	const HorizontalNavList = ({ link }) => {
 		return (
 			<li className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
