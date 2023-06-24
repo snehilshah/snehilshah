f3446784b3d814c5dc377d9ba98d9147571f658c
srshah27 <snehilshah27@gmail.com>
2023-06-24 18:01:18 +0530

    CutomButton :art: Added Custom Button

--- components/Navbar.js
+++ components/Navbar.js
@@ -71,7 +71,7 @@ export default function Navbar() {
 	}
 
 	return (
-		<nav id="nav" className="fixed w-full duration-700 shadow-2xl">
+		<nav id="nav" className="fixed w-full duration-700 shadow-2xl z-50">
 			<div className="flex justify-between items-center w-full h-20 bg-stone-900 text-white px-4">
 				<h1 className="text-5xl ml-2">Snehil</h1>
 				<ul className="hidden md:flex">
