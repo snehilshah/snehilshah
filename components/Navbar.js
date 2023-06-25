338379500366c242b023739a353943949e58f03b
srshah27 <snehilshah27@gmail.com>
2023-06-26 03:14:53 +0530

    :recycle: JSX Single Quotes

--- components/Navbar.js
+++ components/Navbar.js
@@ -29,7 +29,7 @@ export default function Navbar() {
 
   const HorizontalNavList = ({ link }) => {
     return (
-      <li className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200">
+      <li className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
         {link}
       </li>
     )
@@ -37,7 +37,7 @@ export default function Navbar() {
   const VerticalNavList = ({ link }) => {
     return (
       <motion.li
-        className="px-4 capitalize py-6 text-4xl cursor-pointer"
+        className='px-4 capitalize py-6 text-4xl cursor-pointer'
         variants={{
           open: {
             y: 0,
@@ -70,10 +70,10 @@ export default function Navbar() {
   }
 
   return (
-    <nav id="nav" className="fixed w-full duration-700 shadow-2xl z-50">
-      <div className="flex justify-between items-center w-full h-20 bg-stone-950 text-white px-4">
-        <h1 className="text-5xl ml-2">Snehil</h1>
-        <ul className="hidden md:flex">
+    <nav id='nav' className='fixed w-full duration-700 shadow-2xl z-50'>
+      <div className='flex justify-between items-center w-full h-20 bg-stone-950 text-white px-4'>
+        <h1 className='text-5xl ml-2'>Snehil</h1>
+        <ul className='hidden md:flex'>
           {links.map((link, index) => {
             return <HorizontalNavList key={index} link={link} />
           })}
@@ -82,47 +82,47 @@ export default function Navbar() {
           onClick={() => {
             setMobileNav(!mobileNav)
           }}
-          className="cursor-pointer md:hidden text-gray-500 pr-4 z-10"
+          className='cursor-pointer md:hidden text-gray-500 pr-4 z-10'
         >
           <svg
-            width="48"
-            height="48"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+            width='48'
+            height='48'
+            viewBox='0 0 32 32'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
           >
             <motion.rect
               animate={mobileNav ? 'open' : 'closed'}
               variants={topVariants}
               transition={{ duration: 0.7 }}
-              x="6"
-              y="9"
-              width="20"
-              height="2"
-              rx="1"
-              fill="currentColor"
+              x='6'
+              y='9'
+              width='20'
+              height='2'
+              rx='1'
+              fill='currentColor'
             />
             <motion.rect
               animate={mobileNav ? 'open' : 'closed'}
               variants={centerVariants}
               transition={{ duration: 0.4 }}
-              x="6"
-              y="15"
-              width="20"
-              height="2"
-              rx="1"
-              fill="currentColor"
+              x='6'
+              y='15'
+              width='20'
+              height='2'
+              rx='1'
+              fill='currentColor'
             />
             <motion.rect
               animate={mobileNav ? 'open' : 'closed'}
               variants={bottomVariants}
               transition={{ duration: 0.4 }}
-              x="6"
-              y="21"
-              width="20"
-              height="2"
-              rx="1"
-              fill="currentColor"
+              x='6'
+              y='21'
+              width='20'
+              height='2'
+              rx='1'
+              fill='currentColor'
             />
           </svg>
         </div>
@@ -145,12 +145,12 @@ export default function Navbar() {
                   }
                 }
               }}
-              initial="closed"
+              initial='closed'
               animate={'open'}
               exit={'closed'}
-              className="flex flex-col justify-center items-center fixed top-0 left-0 w-full h-3/4 bg-gradient-to-b from-black to-stone-900 opacity-90"
+              className='flex flex-col justify-center items-center fixed top-0 left-0 w-full h-3/4 bg-gradient-to-b from-black to-stone-900 opacity-90'
             >
-              <ul className="text-center">
+              <ul className='text-center'>
                 {links.map((link, index) => {
                   return <VerticalNavList key={index} link={link} />
                 })}
