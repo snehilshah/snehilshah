9482af72f709db8bd52d7b1cf3d515a14d56691d
srshah27 <snehilshah27@gmail.com>
2023-07-03 02:31:07 +0530

    :memo:Added Data for timeline Used map to render timeline

--- components/Timeline/Timeline.js
+++ components/Timeline/Timeline.js
@@ -6,6 +6,36 @@ import 'react-vertical-timeline-component/style.min.css'
 import { FaGraduationCap } from 'react-icons/fa'
 import Heading from '../Heading'
 
+const timelineData = [
+  {
+    date: '2021-Present',
+    header: 'Projects Team',
+    organization: 'Research Cell',
+    qualities: 'Leadership | Discipline',
+    icon: <FaGraduationCap className='text-stone-700' />
+  },
+  {
+    date: '2021-Present',
+    header: 'Projects Team',
+    organization: 'Research Cell',
+    qualities: 'Leadership | Discipline',
+    icon: <FaGraduationCap className='text-stone-700' />
+  },
+  {
+    date: '2021-Present',
+    header: 'Projects Team',
+    organization: 'Research Cell',
+    qualities: 'Leadership | Discipline',
+    icon: <FaGraduationCap className='text-stone-700' />
+  },
+  {
+    date: '2021-Present',
+    header: 'Projects Team',
+    organization: 'Research Cell',
+    qualities: 'Leadership | Discipline',
+    icon: <FaGraduationCap className='text-stone-700' />
+  }
+]
 
 const Timeline = () => {
   return (
@@ -13,25 +43,34 @@ const Timeline = () => {
       <div className='text-white'>
         <Heading title={'Experience'} details={'Events'} />
       </div>
-      <div class='timeline'>
+      <div className='timeline'>
         <VerticalTimeline animate={true} layout='2-columns' lineColor='#0097A7'>
-          <VerticalTimelineElement
-            contentArrowStyle={{ borderRight: '7px solid #0097A7' }}
-            iconStyle={{
-              background: '#a3a3a3',
-              boxShadow: '0 0 0 4px #0097A7'
-            }}
-            date='2021-Present'
-            dateClassName='text-white'
-            icon={<FaGraduationCap className='text-stone-700' />}
-            contentStyle={{
-              background: 'rgb(68 64 60 / 0.6)',
-              color: 'white',
-              boxShadow: '2px 5px 4px 0px #0097A7'
-            }}
-          >
-
-          </VerticalTimelineElement>
+          {timelineData.map(
+            ({ date, header, organization, qualities, icon }, index) => {
+              return (
+                <VerticalTimelineElement
+                  key={index}
+                  contentArrowStyle={{ borderRight: '7px solid #0097A7' }}
+                  iconStyle={{
+                    background: '#a3a3a3',
+                    boxShadow: '0 0 0 4px #0097A7'
+                  }}
+                  date={date}
+                  dateClassName='text-white'
+                  icon={icon}
+                  contentStyle={{
+                    background: 'rgb(68 64 60 / 0.6)',
+                    color: 'white',
+                    boxShadow: '2px 5px 4px 0px #0097A7'
+                  }}
+                >
+                  <h3 className=''>{header}</h3>
+                  <h6 className=''>{organization}</h6>
+                  <p>{qualities}</p>
+                </VerticalTimelineElement>
+              )
+            }
+          )}
         </VerticalTimeline>
       </div>
     </div>
