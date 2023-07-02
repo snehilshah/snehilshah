29d6d9fef9d26e37e7d69566cc3b72edfb032c5d
srshah27 <snehilshah27@gmail.com>
2023-07-03 02:44:07 +0530

    :poop:Corrected text

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -8,15 +8,7 @@ function Footer() {
         <div className='xl:w-1/2 lg:w-3/4 w-full mx-auto text-center flex flex-col justify-center items-center'>
           <BsFillRocketTakeoffFill className='text-5xl mb-10' size={72} />
           <p className='leading-relaxed text-lg'>
-            Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki
-            taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman
-            taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid
-            fanny pack vaporware. Man bun next level coloring book skateboard
-            four loko knausgaard. Kitsch keffiyeh master cleanse direct trade
-            indigo juice before they sold out gentrify plaid gastropub normcore
-            XOXO 90's pickled cindigo jean shorts. Slow-carb next level
-            shoindigoitch ethical authentic, yr scenester sriracha forage
-            franzen organic drinking vinegar.
+            Some Inspiration para
           </p>
           <span className='inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6'></span>
           <h2 className='text-gray-300 font-medium title-font tracking-wider text-sm'>
