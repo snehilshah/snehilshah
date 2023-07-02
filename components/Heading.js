1770bf93aba26e908d9c56c94fcf8741e5cae93c
srshah27 <snehilshah27@gmail.com>
2023-07-03 02:07:56 +0530

    :art:Fixed Heading Padding

--- components/Heading.js
+++ components/Heading.js
@@ -1,7 +1,7 @@
 const Heading = ({ title, details }) => {
   return (
     <div className='flex flex-col items-center lg:block'>
-      <p className='text-4xl font-bold border-b-4 border-stone-500 p-2 inline tracking-wide'>
+      <p className='text-4xl font-bold border-b-4 border-stone-500 p-2 pl-0 inline tracking-wide'>
         {title}
       </p>
       <p className='pb-6 md:py-6'>{details}</p>
