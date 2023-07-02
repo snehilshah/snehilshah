62fb5d4d4de6759cb45f3a038670581c8919da46
srshah27 <snehilshah27@gmail.com>
2023-07-03 02:08:55 +0530

    :art::construction:Fixed small widths overflow Added Max width

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -4,41 +4,48 @@ import Cards from './Cards'
 
 import { motion } from 'framer-motion'
 import { useState, useEffect, useRef } from 'react'
+import Heading from '../Heading'
 
 function Projects() {
   const boxRef = useRef(null)
-
   return (
-    <div
-      ref={boxRef}
-      className='cursor-grab flex overflow-hidden max-w-6xl mx-auto'
-    >
-      <motion.div drag='x' dragConstraints={boxRef} className='flex gap-10'>
-        <div className='h-[40rem] py-10'>
-          <Cards />
-        </div>
-        <div className='h-[40rem] py-10 '>
-          <Cards />
-        </div>
-        <div className='h-[40rem] py-10 '>
-          <Cards />
-        </div>
-        <div className='h-[40rem] py-10 '>
-          <Cards />
-        </div>
-        <div className='h-[40rem] py-10 '>
-          <Cards />
-        </div>
-        <div className='h-[40rem] py-10 '>
-          <Cards />
-        </div>
-        <div className='h-[40rem] py-10 '>
-          <Cards />
-        </div>
-        <div className='h-[40rem] py-10 '>
-          <Cards />
-        </div>
-      </motion.div>
+    <div className='max-w-screen-xl mx-auto text-white mt-16'>
+      <Heading title={'Projects'} details={'Projects I have worked'} />
+      <div
+        ref={boxRef}
+        className='cursor-grab flex overflow-hidden w-full mx-auto p-4'
+      >
+        <motion.div
+          drag='x'
+          dragConstraints={boxRef}
+          className='flex gap-10 pl-2'
+        >
+          <div className='h-[40rem] py-10'>
+            <Cards />
+          </div>
+          <div className='h-[40rem] py-10 '>
+            <Cards />
+          </div>
+          <div className='h-[40rem] py-10 '>
+            <Cards />
+          </div>
+          <div className='h-[40rem] py-10 '>
+            <Cards />
+          </div>
+          <div className='h-[40rem] py-10 '>
+            <Cards />
+          </div>
+          <div className='h-[40rem] py-10 '>
+            <Cards />
+          </div>
+          <div className='h-[40rem] py-10 '>
+            <Cards />
+          </div>
+          <div className='h-[40rem] py-10 '>
+            <Cards />
+          </div>
+        </motion.div>
+      </div>
     </div>
   )
 }
