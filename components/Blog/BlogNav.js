56098c7696d0f268ed1165800de2f3b6d7334117
srshah27 <snehilshah27@gmail.com>
2023-07-09 21:45:46 +0530

    :pencil2: changed `class` to `className`

--- components/Blog/BlogNav.js
+++ components/Blog/BlogNav.js
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 export default function BlogNav() {
   return (
-    <header class='p-4 flex justify-center relative bg-stone-900'>
+    <header className='p-4 flex justify-center relative bg-stone-900'>
       <NavBarLogo />
       <Link href={'/'}>
-        <button class='text-stone-300 inline-flex items-center bg-stone-500 py-1 px-3 rounded mt-4 md:mt-0 absolute right-5 top-6'>
+        <button className='text-stone-300 inline-flex items-center bg-stone-500 py-1 px-3 rounded mt-4 md:mt-0 absolute right-5 top-6'>
           Portfolio
           <svg
             fill='none'
@@ -14,7 +14,7 @@ export default function BlogNav() {
             stroke-linecap='round'
             stroke-linejoin='round'
             stroke-width='2'
-            class='w-4 h-4 ml-1'
+            className='w-4 h-4 ml-1'
             viewBox='0 0 24 24'
           >
             <path d='M5 12h14M12 5l7 7-7 7'></path>
