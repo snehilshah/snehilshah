bb902c8a5c77b2744bb63935f867d95c2c04ab1f
srshah27 <snehilshah27@gmail.com>
2023-07-13 23:26:03 +0530

    :memo: BlogHero Text Update

--- components/Blog/BlogHero.js
+++ components/Blog/BlogHero.js
@@ -4,10 +4,10 @@ const BlogHero = () => {
       <div className='container px-5 py-24 mx-auto'>
         <div className='xl:w-1/2 lg:w-3/4 w-full mx-auto text-center'>
           <h3 className='leading-relaxed text-3xl text-stone-950'>
-            Lets Explore Discover and Connect
+            Lets Explore Discover and Stay Updated
           </h3>
           <h3 className='leading-relaxed text-lg text-stone-950'>
-            Find the latest of my writing here
+            on the fascinating world of technology together
           </h3>
           <span className='inline-block h-1 w-10 rounded bg-stone-500 mt-8'></span>
         </div>
