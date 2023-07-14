1831626f5192508ff9c7839d716fe818462a426e
srshah27 <snehilshah27@gmail.com>
2023-07-14 18:45:12 +0530

    :pencil2: SVG elements to React

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -43,7 +43,7 @@ function blogs({ posts }) {
                   <p className='text-sm'>{post.frontmatter.description}</p>
                 </div>
 
-                <p className='min-w-[5rem]'>{post.frontmatter.date}</p>
+                <p className='min-w-[5rem] whitespace-nowrap'>{post.frontmatter.date}</p>
               </div>
             </div>
           </Link>
