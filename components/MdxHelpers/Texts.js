504e56e0b84b96ffa9e54d0e564aae18755d02ce
srshah27 <snehilshah27@gmail.com>
2023-08-05 04:36:45 +0530

    :pencil2: StandOut to Standout

--- components/MdxHelpers/Texts.js
+++ components/MdxHelpers/Texts.js
@@ -1,4 +1,4 @@
-export const StandOut = ({ text }) => {
+export const Standout = ({ text }) => {
   return (
     <span className='bg-clip-text text-transparent bg-gradient-to-r from-fuchsia-600 to-rose-600'>
       <b>{text}</b>
