09cb40b13d224bd92c75872bbe02ca8b2ff8865a
srshah27 <snehilshah27@gmail.com>
2024-10-13 05:14:52 +0530

    :sparkles: new projects carousel

--- components/Index/Projects/Cards.js
+++ components/Index/Projects/Cards.js
@@ -1,18 +1,19 @@
 import Image from 'next/image'
 import styles from '../../../styles/Card.module.css'
 import clsx from 'clsx'
-function Cards({ title, desc, src, classes }) {
+function Cards({ title, desc, src, classes, bgColor }) {
   return (
-    <div className={clsx(styles.card + ' my-10 mr-2 ' + classes)}>
+    <div class='hero'>
       <Image
         src={src}
-        className='overflow-hidden object-cover'
+        className='aspect-square object-cover h-full w-full'
         alt='Project Background'
       />
-      <div className={clsx(styles.card__content) + ' '}>
-        <p className={clsx(styles.card__title) + ''}>{title}</p>
-        <p className={clsx(styles.card__description) + ''}>{desc}</p>
+      <div className={`text bg-gradient-to-br ${bgColor}`}></div>
+      <div class='main-text'>
+        <p className={`${classes}`}>{title}</p>
       </div>
+      <p className=''>{desc}</p>
     </div>
   )
 }
--- components/Index/Projects/Projects.js
+++ components/Index/Projects/Projects.js
@@ -1,81 +1,150 @@
-import Cards from './Cards'
-import { motion } from 'framer-motion'
 import { useRef } from 'react'
 import Heading from '../Heading'
 import StockScreen from '../../../public/projects/stockscreen.webp'
 import AgendaBuilder from '../../../public/projects/agenda.webp'
-import IDF from '../../../public/projects/balgurukul.webp'
+import Balgurukul from '../../../public/projects/balgurukul.webp'
 import ProjectGallery from '../../../public/projects/homebackground.webp'
 import HyperSpectral from '../../../public/projects/hypersceptral.webp'
 import Heart from '../../../public/projects/heart.png'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem, 
+  CarouselNext,
+  CarouselPrevious
+} from '../../ui/carousel'
+import { SquareChevronLeft, SquareChevronRight } from 'lucide-react'
+import { useEffect } from 'react'
+import Cards from './Cards'
 
-function Projects() {
-  const boxRef = useRef(null)
-  const projects = [
-    {
-      title: 'Stock Analysis',
-      desc: 'This is the project description',
-      bgImage: StockScreen,
-      classes: ''
-    },
-    {
-      title: 'Agenda Builder',
-      desc: 'This is the project description',
-      bgImage: AgendaBuilder,
-      classes: 'text-black'
-    },
-    {
-      title: 'IDF Balgurukul',
-      desc: 'This is the project description',
-      bgImage: IDF,
-      classes: 'text-black'
-    },
-    {
-      title: 'Project Gallery',
-      desc: 'This is the project description',
-      bgImage: ProjectGallery,
-      classes: 'text-black'
-    },
-    {
-      title: 'Hyperspectral Image',
-      desc: 'This is the project description',
-      bgImage: HyperSpectral,
-      classes: 'text-black'
-    },
-    {
-      title: 'Portfolio Website',
-      desc: 'This is the project description',
-      bgImage: Heart,
-      classes: ''
+const projects = [
+  {
+    title: 'Stock Analysis',
+    desc: 'This is the project description',
+    classes: '',
+    bgColor: 'from-amber-600 to-neutral-900',
+    imagePath: StockScreen
+  },
+  {
+    title: 'Agenda Builder',
+    desc: 'This is the project description',
+    classes: '',
+    // bgColor: 'from-violet-500 to-pink-500',
+    bgColor: 'from-purple-500 via-pink-500 to-red-500',
+    imagePath: AgendaBuilder
+  },
+  {
+    title: 'IDF Balgurukul',
+    desc: 'This is the project description',
+    classes: '',
+    bgColor: 'from-blue-600 to-cyan-500',
+    imagePath: Balgurukul
+  },
+  {
+    title: 'Project Gallery',
+    desc: 'This is the project description',
+    classes: 'text-black',
+    bgColor: 'from-white via-neutral-700 to-black text-black',
+    imagePath: ProjectGallery
+  },
+  {
+    title: 'Hyperspectral Image',
+    desc: 'This is the project description',
+    classes: 'text-black',
+    bgColor: 'from-green-500 via-teal-500 to-blue-500',
+    imagePath: HyperSpectral
+  },
+  {
+    title: 'Portfolio Website',
+    desc: 'This is the project description',
+    classes: '',
+    bgColor: 'from-neutral-600 via-neutral-800 to-rose-600',
+    imagePath: Heart
+  }
+]
+export default function Projects() {
+  const carouselRef = useRef(null)
+
+  useEffect(() => {
+    const carousel = carouselRef.current
+    if (!carousel) return
+
+    let isDown = false
+    let startX
+    let scrollLeft
+
+    const onMouseDown = e => {
+      isDown = true
+      carousel.classList.add('active')
+      startX = e.pageX - carousel.offsetLeft
+      scrollLeft = carousel.scrollLeft
     }
-  ]
+
+    const onMouseLeave = () => {
+      isDown = false
+      carousel.classList.remove('active')
+    }
+
+    const onMouseUp = () => {
+      isDown = false
+      carousel.classList.remove('active')
+    }
+
+    const onMouseMove = e => {
+      if (!isDown) return
+      e.preventDefault()
+      const x = e.pageX - carousel.offsetLeft
+      const walk = (x - startX) * 2
+      carousel.scrollLeft = scrollLeft - walk
+    }
+
+    carousel.addEventListener('mousedown', onMouseDown)
+    carousel.addEventListener('mouseleave', onMouseLeave)
+    carousel.addEventListener('mouseup', onMouseUp)
+    carousel.addEventListener('mousemove', onMouseMove)
+    return () => {
+      carousel.removeEventListener('mousedown', onMouseDown)
+      carousel.removeEventListener('mouseleave', onMouseLeave)
+      carousel.removeEventListener('mouseup', onMouseUp)
+      carousel.removeEventListener('mousemove', onMouseMove)
+    }
+  }, [])
+
   return (
     <div className='max-w-screen-xl mx-auto text-white my-32' id='projects'>
       <Heading title={'Projects'} details={'Projects I have worked'} />
-      <div
-        ref={boxRef}
-        className='cursor-grab flex overflow-hidden w-full mx-auto p-4'
-      >
-        <motion.div
-          drag='x'
-          dragConstraints={boxRef}
-          className='flex gap-8 pl-2'
+      <Carousel className='w-full'>
+        <div className='flex justify-between items-center'>
+          <div className='flex space-x-2'>
+            <CarouselPrevious className='relative top-8 left-12 translate-x-0 translate-y-0 h-8 w-8 bg-transparent text-gray-300 '>
+              <SquareChevronLeft className='h-4 w-4' />
+            </CarouselPrevious>
+            <CarouselNext className='relative top-8 translate-x-0 translate-y-0 h-8 w-8 bg-transparent text-gray-300 '>
+              <SquareChevronRight className='h-4 w-4' />
+            </CarouselNext>
+          </div>
+        </div>
+        <CarouselContent
+          ref={carouselRef}
+          className='cursor-grab active:cursor-grabbing mt-5'
         >
-          {projects.map((project, index) => {
-            return (
+          {projects.map((item, index) => (
+            <CarouselItem
+              key={index}
+              className='md:basis-1/2 lg:basis-1/3 pl-4'
+            >
               <Cards
                 key={index}
-                title={project.title}
-                desc={project.desc}
-                src={project.bgImage}
-                classes={project.classes}
-              />
-            )
-          })}
-        </motion.div>
-      </div>
+                title={item.title}
+                desc={item.desc}
+                src={item.imagePath}
+                bgColor={item.bgColor}
+                classes={item.classes}
+              ></Cards>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   )
 }
-
-export default Projects
