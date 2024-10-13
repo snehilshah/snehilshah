ca60da2749513ca91b56196b2a721307abad320b
srshah27 <snehilshah27@gmail.com>
2024-10-13 07:34:58 +0530

    :construction: spinner animation

--- components/Index/animations/OrbitingCircles.js
+++ components/Index/animations/OrbitingCircles.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export function OrbitingCirclesComponent() {
   return (
     <div className='relative flex h-[500px] w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg'>
-      <span className='pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-500/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10'>
+      <span className='pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-500/80 bg-clip-text text-center text-4xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10'>
         Happy Weekend Sir!!
       </span>
 
