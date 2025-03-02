d7a351c6e7780ebbea2d7aca8b81c8b631863cac
snehilshah <snehilshah27@gmail.com>
2025-03-02 07:22:10 +0530

    :art: compliment header

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -72,12 +72,12 @@ function PostPage({
           className='w-full h-[30rem] object-cover rounded-3xl'
         />
       </div>
-      <article className='font-copernicus max-w-4xl mx-auto p-5 prose prose-lg prose-colorful prose-a:text-emerald-500 text-justify'>
-        <h1 className='text-4xl mt-10 text-center font-bold mb-4 bg-gradient-to-r from-[#59321A] to-[#6f3f21d7] text-transparent bg-clip-text'>
+      <article className='font-copernicus max-w-4xl mx-auto p-5 prose prose-lg prose-colorful prose-a:text-[#21516F] text-justify'>
+        <h1 className='text-4xl mt-10 text-center font-bold mb-4 bg-gradient-to-r text-[#59321A]'>
           {title}
         </h1>
-        <div className='w-24 h-1 bg-[#59321A] mx-auto mb-6'></div>
-        <h2 className='text-xl font-light mb-8 mt-1 text-center italic text-[#6F6621]'>
+        <hr className='w-24 h-1 bg-[#59321A] mx-auto my-4 border-0' />
+        <h2 className='text-xl font-light mb-8 mt-1 text-center italic text-[#4f2b15]'>
           {description}
         </h2>
         <MDXRemote {...mdxSource} components={components} />
