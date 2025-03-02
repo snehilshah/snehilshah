3706211df6e3e454a6e25a6419d21f57afd6aa15
snehilshah <snehilshah27@gmail.com>
2025-03-02 11:02:41 +0530

    :muscle: cleanup

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,16 @@
-import "@/styles/globals.tail.css";
-import { supreme, prsans } from '@/styles/fonts';
-import { Metadata } from 'next'
+import '@/styles/globals.tail.css'
+import { supreme, prsans } from '@/styles/fonts'
 
-const fontClasses = [supreme.variable, prsans.variable].join(' ');
+const fontClasses = [supreme.variable, prsans.variable].join(' ')
 export const metadata = {
-  title: "Snehil Shah | Portfolio",
-  description: "Portfolio Website for Snehil Shah",
-};
+  title: 'Snehil Shah | Portfolio',
+  description: 'Portfolio Website for Snehil Shah'
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="">
-      <body className={`${fontClasses} font-prsans`}>
-        {children}
-      </body>
+    <html lang='en' className=''>
+      <body className={`${fontClasses} font-prsans`}>{children}</body>
     </html>
   )
 }
