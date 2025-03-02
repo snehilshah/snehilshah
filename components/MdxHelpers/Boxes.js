6813dcd5a557c34e47ca9ff20723b2048b4c9bda
snehilshah <snehilshah27@gmail.com>
2025-03-02 06:22:17 +0530

    :art: complimentary colors for boxes

--- components/MdxHelpers/Boxes.js
+++ components/MdxHelpers/Boxes.js
@@ -6,7 +6,9 @@ export const ErrorBox = ({ text }) => {
   return (
     <div className='bg-rose-700 rounded-xl text-white mb-6 px-3 py-2 flex items-center gap-4'>
       <OctagonX size={30} />
-      <span className='text-center w-full'>{text}</span>
+      <span className='text-center w-full font-product text-rose-200'>
+        {text}
+      </span>
     </div>
   )
 }
@@ -15,7 +17,9 @@ export const InfoBox = ({ text }) => {
   return (
     <div className='bg-sky-700 rounded-xl text-white mb-6 px-3 py-2 flex items-center gap-4'>
       <Info size={30} />
-      <span className='text-center w-full'>{text}</span>
+      <span className='text-center w-full font-product text-sky-200'>
+        {text}
+      </span>
     </div>
   )
 }
@@ -24,7 +28,9 @@ export const WarningBox = ({ text }) => {
   return (
     <div className='bg-amber-500 rounded-xl text-white mb-6 px-3 py-2 flex items-center gap-4'>
       <CircleAlert size={30} />
-      <span className='text-center w-full'>{text}</span>
+      <span className='text-center w-full font-product text-amber-950'>
+        {text}
+      </span>
     </div>
   )
 }
@@ -44,17 +50,17 @@ export const ImageBox = ({ link, text = '' }) => {
 
 export const StyleBox = props => {
   return (
-    <>
-      <div className='relative w-4/5 h-[100px] mx-auto overflow-hidden bg-[#ffe4c4] rounded-[15px] flex z-[1] shadow-[0_0_30px_5px_rgba(0,0,0,0.2)]'>
-        <div className='relative z-[5] w-full h-full flex items-center justify-center rounded-[15px] backdrop-filter backdrop-blur-[40px]'>
-          <div className='z-50 text-center'>{props.children}</div>
+    <div className='relative w-4/5 h-[100px] mx-auto overflow-hidden bg-[#ffe4c4] rounded-[15px] flex z-[1] shadow-[0_0_30px_5px_rgba(0,0,0,0.2)]'>
+      <div className='relative z-[5] w-full h-full flex items-center justify-center rounded-[15px] backdrop-filter backdrop-blur-[40px]'>
+        <div className='font-product text-amber-950 text-base z-50 text-center p-4'>
+          {props.children}
         </div>
-        <div className='absolute w-[230px] h-[230px] rounded-full z-[2] left-0 bg-[#ffb84c] animate-wave [animation-delay:0s]'></div>
-        <div className='absolute w-[230px] h-[230px] rounded-full z-[2] top-0 left-[100px] bg-[#f266ab] animate-wave [animation-delay:0.5s]'></div>
-        <div className='absolute w-[230px] h-[230px] rounded-full z-[2] top-[10px] left-[200px] bg-[#af71d38b] animate-wave [animation-delay:1s]'></div>
-        <div className='absolute w-[230px] h-[230px] rounded-full z-[2] top-0 left-[350px] bg-[#2cd3e1] animate-wave [animation-delay:1.5s]'></div>
       </div>
-    </>
+      <div className='absolute w-[230px] h-[230px] rounded-full z-[2] left-0 bg-[#ffb84c] animate-wave [animation-delay:0s]'></div>
+      <div className='absolute w-[230px] h-[230px] rounded-full z-[2] top-0 left-[100px] bg-[#f266ab] animate-wave [animation-delay:0.5s]'></div>
+      <div className='absolute w-[230px] h-[230px] rounded-full z-[2] top-[10px] left-[200px] bg-[#af71d38b] animate-wave [animation-delay:1s]'></div>
+      <div className='absolute w-[230px] h-[230px] rounded-full z-[2] top-0 left-[350px] bg-[#2cd3e1] animate-wave [animation-delay:1.5s]'></div>
+    </div>
   )
 }
 
